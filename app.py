d6f7da7e23ea0410cd0bd4e5bae53e8323c6fa77
Fermin Cirella <letrix@outlook.es>
2020-06-03 17:30:19 -0300

    fixes

--- app.py
+++ app.py
@@ -107,4 +107,4 @@ def index():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000, debug=True)
+    app.run(threaded=True, port=5000, debug=False)
